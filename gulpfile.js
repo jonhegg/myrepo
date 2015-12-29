8375684f732d0c1bd4afd7a16742e8a89a66e302
Jon Hegg <jh@locus.no>
2015-12-29 15:24:04 +0100

    Set up Handlebars

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,18 @@ var minifyCss = require('gulp-minify-css');
 var sourceMaps = require('gulp-sourcemaps');
 var ugliy = require('gulp-uglify');
 var imageMin = require('gulp-imagemin');
+var handleBars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
+
+gulp.task('templates', function() {
+  return gulp.src(['src/templates/**/*.hbs'])
+  .pipe(handleBars())
+  .pipe(rename(function(path){
+    path.extname = '.html'
+  }))
+  .pipe(gulp.dest('./'));
+})
+
 
 gulp.task('images', function(){
     gulp.src(['src/img/**/*'])
@@ -33,7 +45,7 @@ gulp.task('styles', function(){
 
 });
 
-gulp.task('default', function(){
+gulp.task('default',['styles','images','scripts','templates'], function(){
   browserSync.init({
     server: './'
   });
@@ -41,5 +53,7 @@ gulp.task('default', function(){
   gulp.watch('src/styles/**/*.css',['styles']);
   gulp.watch('src/scripts/**/*.js',['scripts']);
   gulp.watch('src/img/**/*',['images']);
+  gulp.watch('src/templates/**/*.hbs',['templates']);
+
   gulp.watch('*.html',browserSync.reload);
 });
