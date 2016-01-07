de5c5b44dbfbd16bc4634e55cc0237653180f8d4
Jon Hegg <jh@locus.no>
2016-01-07 15:26:45 +0100

    Menu stuff

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,14 @@ var imageMin = require('gulp-imagemin');
 var handleBars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
 
+var menu = require('./menu.json');
+
+
 gulp.task('templates', function() {
-  var data = {};
+  var data = {
+    year: new Date().getFullYear(),
+    menu: menu.menuItems
+  };
 
   var options = {
     batch: ['src/templates/partials']
